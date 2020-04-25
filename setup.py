490551167b4043cd615cf0e3ca7660dc1960b71a
Pierre Berger <pierrelinkin@yahoo.fr>
2020-04-25 16:47:40 +0200

    add Metromobilite class

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="metromobilite",
+    version="0.0.1",
+    description="Python library for metromobilite API",
+    author="PierreBerger",
+    license="MIT",
+    url="https://github.com/PierreBerger/metromobilite",
+    packages=find_packages(),
+)
